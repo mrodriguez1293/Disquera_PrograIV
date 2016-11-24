bfc828a79a69162a46a2981867d8f8e0bc67d0ff
Matias Rodriguez Corvalan <mrodriguez1293@icloud.com>
2016-11-24 16:32:50 -0300

    Agregar archivos de proyecto.

---
+++ Disquera_PrograIV/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Disquera_PrograIV.MvcApplication" Language="C#" %>
---
+++ Disquera_PrograIV/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Disquera_PrograIV.Startup))]
+namespace Disquera_PrograIV
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
